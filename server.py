de25d021b2cbcd5fa4500eaec4962c4f88ab8499
Nicolukazzz <mamiann42@gmail.com>
2024-11-20 23:53:58 -0500

    Update get

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 import os
+
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return 'Hello from Flask!'
+UPLOAD_FOLDER = './uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)  # Crea la carpeta si no existe
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -14,10 +13,15 @@ def upload_file():
     file = request.files['file']
     if file.filename == '':
         return "No selected file", 400
-    file.save(f"./uploaded_{file.filename}")  # Guarda el archivo en el servidor
+    file.save(os.path.join(UPLOAD_FOLDER, f"uploaded_{file.filename}"))
     return "File uploaded successfully", 200
 
+@app.route('/files/<filename>', methods=['GET'])
+def get_file(filename):
+    try:
+        return send_from_directory(UPLOAD_FOLDER, filename)
+    except FileNotFoundError:
+        return "File not found", 404
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Usa el puerto asignado por Railway
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
